e95308c739bff2423c2217512e394e5eb0deb47e
Brad Hoza <10886356+bradhoza@users.noreply.github.com>
2026-09-17 22:13:30 -0700

    add *.png to filter for 3-convert-to-16x9.sh

--- 3-convert-to-16x9.sh
+++ 3-convert-to-16x9.sh
@@ -5,7 +5,7 @@ input_dir="input"
 output_dir="output"
 
 shopt -s nocaseglob
-for img in "$input_dir"/*.{jpg,jpeg}; do
+for img in "$input_dir"/*.{jpg,jpeg,png}; do
     echo "Processing $img"
     output_file="$output_dir/$(basename "$img")"
     magick "$img" \
